3f094f212505882caacc7aa4cd1c9e9a877d27c1
Martin Clauß <mc@cs.uni-bonn.de>
2023-08-31 15:34:25 +0200

    make Docker build output more visible

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 # this will run on the host (not inside the container)
 
-docker build -t aflgo .
+BUILDKIT_PROGRESS=plain docker build -t aflgo .
 # we need --privileged to change /proc and /sys settings for AFLGo
 docker run -it --rm --privileged -v $(pwd):/io:Z aflgo ./setup_afl_and_fuzz.sh
